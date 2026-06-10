83bfaf3f4934b6ea188d650c652f4438ac00803c
SHANDINI M <shandinim.ece2024@citchennai.net>
2026-06-10 08:46:36 +0530

    Add docstrings to add, sub, mul, and div functions
    Added docstrings to arithmetic functions.

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
 def add(a,b):
-  return a+b
+    """add two numbers"""
+    return a+b
 def sub(a,b):
-  return a-b
+    """subtract two"""
+    return a-b
 def mul(a,b):
-  return a*b
+    """multiply two numbers"""
+    return a*b
 def div(a,b):
+    """division  two numbers"""
   if b==0:
     raise ValueError("division by zero")
   return a/b
